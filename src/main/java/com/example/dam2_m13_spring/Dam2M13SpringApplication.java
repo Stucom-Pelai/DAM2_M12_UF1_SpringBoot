e804e6e810658ee9eb8eb6fa98a33abe16e78d8b
JPortugal <jose.portugal@stucom.com>
2024-05-02 22:28:39 +0200

    adds docker file

--- src/main/java/com/example/dam2_m13_spring/Dam2M13SpringApplication.java
+++ src/main/java/com/example/dam2_m13_spring/Dam2M13SpringApplication.java
@@ -7,10 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Class to start the application
  */
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan(basePackages = "entity")
-public class Dam2M13SpringApplication {
+public class Dam2M13SpringApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Dam2M13SpringApplication.class, args);
